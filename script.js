18117ae81069fa5dc94fbf9b2875853eeb8ea370
huzefarana <imhuzefarana@gmail.com>
2022-08-03 01:14:03 -0700

    push

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const checkWin = ()=>{
     let wins = [
         [0,1,2,3], [4,5,6,7], [8,9,10,11], [12,13,14,15],
         [0,4,8,12], [1,5,9,13], [2,6,10,14], [3,7,11,15],
-        [0,5,10,15], [3,6,10,12]
+        [0,5,10,15], [3,6,9,12]
     ];
     wins.forEach(e => {
         if( boxtext[e[0]].innerText!=='',
@@ -47,7 +47,7 @@ boxes.forEach(element =>{
             turn = changeTurn();
             document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
             checkWin();
-            music.play()
+            //music.play()
             gameOver()
         }
     })
