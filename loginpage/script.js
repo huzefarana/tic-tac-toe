128d2fa72e442eb6178c4e657253535ad47c9b53
huzefarana <95310713+huzefarana@users.noreply.github.com>
2022-08-06 21:59:26 +0500

    Update script.js

--- loginpage/script.js
+++ loginpage/script.js
@@ -1,28 +1,20 @@
-
-let credentials = [
-  {Name1:"bilal" , password1:"markcoders"},
-  {Name2:"huzefa", password2:"abc123"},
-  {Name3:"saram",  password3:"saram90"}
-]
-
+var myName = "bilal"
+var input = (document.getElementById("name").innerText)
 function validate(){
-  if((document.getElementById("name").innerText === credentials.Name1) &&
-  (document.getElementById("password").innerText === credentials.password1) ||
-  (document.getElementById("name").innnerText === credentials.Name2) &&
-  (document.getElementById("password").innerText === credentials.password2) || 
-  (document.getElementById("name").innerText === credentials.Name3) && 
-  (document.getElementById("password").innerText === credentials.password3) &&
-  (document.getElementById("input").innerText !== "")){
-    alert("login Successfully")   
+  if(( input == myName) &&
+  // (document.getElementById("password").innerText === credentials.password1) ||
+  // (document.getElementById("name").innnerText === credentials.Name2) &&
+  // (document.getElementById("password").innerText === credentials.password2) || 
+  // (document.getElementById("name").innerText === credentials.Name3) && 
+  // (document.getElementById("password").innerText === credentials.password3) &&
+  (input !== "")){
+    alert("login Successfully")
+    
     }
  else{
   alert("Login failed ")
  }
 }
-document.getElementById("myButton").onclick = function (){
-  location. href = "dashboard.html"
- }
-
 
 
 
