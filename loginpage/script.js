49d5ebf38bca4c89e44d67192e100e01e44cecfd
huzefarana <imhuzefarana@gmail.com>
2022-08-09 22:02:41 +0500

    update

--- loginpage/script.js
+++ loginpage/script.js
@@ -1,69 +1,14 @@
-<<<<<<< HEAD
 
-// var myName = "bilal"
-// var input = (document.getElementById("name").innerHTML)
-// function validate(){
-//   if(( input == myName) &&
-//   // (document.getElementById("password").innerText === credentials.password1) ||
-//   // (document.getElementById("name").innnerText === credentials.Name2) &&
-//   // (document.getElementById("password").innerText === credentials.password2) || 
-//   // (document.getElementById("name").innerText === credentials.Name3) && 
-//   // (document.getElementById("password").innerText === credentials.password3) &&
-//   (input !== "")){
-//     alert("login Successfully")
-=======
 var myName = "bilal"
 var input = (document.getElementById("name").innerText)
 function validate(){
   if(( input == myName) &&
-  // (document.getElementById("password").innerText === credentials.password1) ||
-  // (document.getElementById("name").innnerText === credentials.Name2) &&
-  // (document.getElementById("password").innerText === credentials.password2) || 
-  // (document.getElementById("name").innerText === credentials.Name3) && 
-  // (document.getElementById("password").innerText === credentials.password3) &&
+
   (input !== "")){
     alert("login Successfully")
->>>>>>> 128d2fa72e442eb6178c4e657253535ad47c9b53
     
-//     }
-//  else{
-//   alert("Login failed")
-//  }
-// }
-
-let num1 = 8
-let num2 = 2
-
-let sum = document.getElementById("sum-el")
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
-
-
-function add() {
-  let result = num1 + num2
-  sum.innerText = "output:" + result
+    }
+ else{
+  alert("Login failed")
+ }
 }
-function subtract() {
-  let result = num1 - num2
-  sum.innerText = "output:" + result
-}
-function multiply() {
-  let result = num1 * num2
-  sum.innerText = "output:" + result
-}
-function divide() {
-  let result = num1 / num2
-  sum.innerText = "output:" + result
-}
-
-<<<<<<< HEAD
-function reset() {
-  sum.textContent = ""
-}
-=======
-
-
-
-
-
->>>>>>> 128d2fa72e442eb6178c4e657253535ad47c9b53
