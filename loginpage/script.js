9a4bdb384882510a7eb1d834ccb94f662a3ac459
huzefarana <95310713+huzefarana@users.noreply.github.com>
2022-08-09 21:59:03 +0500

    Update script.js

--- loginpage/script.js
+++ loginpage/script.js
@@ -31,36 +31,36 @@ function validate(){
 //  }
 // }
 
-let num1 = 8
-let num2 = 2
+//let num1 = 8
+//let num2 = 2
 
-let sum = document.getElementById("sum-el")
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+//let sum = document.getElementById("sum-el")
+//document.getElementById("num1-el").textContent = num1
+//document.getElementById("num2-el").textContent = num2
 
 
-function add() {
-  let result = num1 + num2
-  sum.innerText = "output:" + result
-}
-function subtract() {
-  let result = num1 - num2
-  sum.innerText = "output:" + result
-}
-function multiply() {
-  let result = num1 * num2
-  sum.innerText = "output:" + result
-}
-function divide() {
-  let result = num1 / num2
-  sum.innerText = "output:" + result
-}
+//function add() {
+//  let result = num1 + num2
+//  sum.innerText = "output:" + result
+//}
+//function subtract() {
+ // let result = num1 - num2
+ // sum.innerText = "output:" + result
+//
+//function multiply() {
+//  let result = num1 * num2
+//  sum.innerText = "output:" + result
+//}
+//function divide() {
+//  let result = num1 / num2
+//  sum.innerText = "output:" + result
+//}
 
 <<<<<<< HEAD
-function reset() {
-  sum.textContent = ""
-}
-=======
+//function reset() {
+//  sum.textContent = ""
+//}
+//=======
 
 
 
