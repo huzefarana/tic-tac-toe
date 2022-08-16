392271205787beb0c6a1da2c2db557c5c72e2a7a
huzefarana <imhuzefarana@gmail.com>
2022-08-16 01:33:53 -0700

    working

--- loginpage/script.js
+++ loginpage/script.js
@@ -1,26 +1,26 @@
 
-// let credentials = [
-//   {Name1:"bilal" , password1:"markcoders"},
-//   {Name2:"huzefa", password2:"abc123"},
-//   {Name3:"saram",  password3:"saram90"} 
-// ]
-
-  var myName = "bilal"  
-  var input = (document.getElementById("name").innerText)
-  
+let credentials = [
+  {Name1:"bilal" , password1:"markcoders"},
+  {Name2:"huzefa", password2:"abc123"},
+  {Name3:"sarang",  password3:"sarang90"} 
+]
 
 function validate(){
-  if((input == myName) 
-  // (document.getElementById("password").innerText == credentials[0].password1) 
-  // (document.getElementById("name").innnerText == credentials[1].Name2) &&
-  // (document.getElementById("password").innerText == credentials[1].password2) || 
-  // (document.getElementById("name").innerText == credentials[2].Name3) && 
-  // (document.getElementById("password").innerText == credentials[2].password3) &&
-  &&(input !== "")){
+  var input1 = (document.getElementById("name").value)
+  var input2 = (document.getElementById("password").value)
+
+  if((input1 == credentials[0].Name1) &&
+     (input2 == credentials[0].password1) ||
+     (input1 == credentials[1].Name2) &&
+     (input2 == credentials[1].password2) ||
+     (input1 == credentials[2].Name3) &&
+     (input2 == credentials[2].password3) &&
+     (input1 && input2 !== "")
+){
     alert("login Successfully")   
     }
  else{
-  alert("Login failed ")
+  alert("Login failed")
  }
 }
 
