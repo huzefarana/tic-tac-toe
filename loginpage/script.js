29a1fe750a8e0bd1b3e75b3fe7ddee7695d17fa4
huzefarana <imhuzefarana@gmail.com>
2022-08-16 00:30:57 -0700

    hello

--- loginpage/script.js
+++ loginpage/script.js
@@ -1,27 +1,30 @@
 
-let credentials = [
-  {Name1:"bilal" , password1:"markcoders"},
-  {Name2:"huzefa", password2:"abc123"},
-  {Name3:"saram",  password3:"saram90"}
-]
+// let credentials = [
+//   {Name1:"bilal" , password1:"markcoders"},
+//   {Name2:"huzefa", password2:"abc123"},
+//   {Name3:"saram",  password3:"saram90"} 
+// ]
+
+  var myName = "bilal"  
+  var input = (document.getElementById("name").innerText)
+  
 
 function validate(){
-  if((document.getElementById("name").innerText === credentials.Name1) &&
-  (document.getElementById("password").innerText === credentials.password1) ||
-  (document.getElementById("name").innnerText === credentials.Name2) &&
-  (document.getElementById("password").innerText === credentials.password2) || 
-  (document.getElementById("name").innerText === credentials.Name3) && 
-  (document.getElementById("password").innerText === credentials.password3) &&
-  (document.getElementById("input").innerText !== "")){
+  if((input == myName) 
+  // (document.getElementById("password").innerText == credentials[0].password1) 
+  // (document.getElementById("name").innnerText == credentials[1].Name2) &&
+  // (document.getElementById("password").innerText == credentials[1].password2) || 
+  // (document.getElementById("name").innerText == credentials[2].Name3) && 
+  // (document.getElementById("password").innerText == credentials[2].password3) &&
+  &&(input !== "")){
     alert("login Successfully")   
     }
  else{
   alert("Login failed ")
  }
 }
-document.getElementById("myButton").onclick = function (){
-  location. href = "dashboard.html"
- }
+
+
 
 
 
