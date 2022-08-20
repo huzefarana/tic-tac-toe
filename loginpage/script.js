a1f6feea816583673dbb195ec1c5d1b9731e9603
huzefarana <imhuzefarana@gmail.com>
2022-08-20 01:03:31 -0700

    update

--- loginpage/script.js
+++ loginpage/script.js
@@ -1,25 +1,45 @@
 
 let credentials = [
-  {Name1:"bilal" , password1:"markcoders"},
-  {Name2:"huzefa", password2:"abc123"},
-  {Name3:"sarang",  password3:"sarang90"} 
+  {name:"bilal" , password:"markcoders"},
+  {name:"huzefa", password:"abc123"},
+  {name:"sarang", password:"sarang90"} 
 ]
 
-function validate(){
+//   var input1 = (document.getElementById("name").value)
+//   var input2 = (document.getElementById("password").value)
+
+//   if((input1 == credentials[0].Name1) &&
+//      (input2 == credentials[0].password1) ||
+//      (input1 == credentials[1].Name2) &&
+//      (input2 == credentials[1].password2) ||
+//      (input1 == credentials[2].Name3) &&
+//      (input2 == credentials[2].password3) &&
+//      (input1 && input2 !== "")
+// ){
+//     alert("login Successfully")   
+//     }
+//  else{
+//   alert("Login failed")
+//  }
+// }
+
+localStorage.setItem(current_user.name == input1)
+
+check();
+function check()
+{
+
   var input1 = (document.getElementById("name").value)
   var input2 = (document.getElementById("password").value)
 
-  if((input1 == credentials[0].Name1) &&
-     (input2 == credentials[0].password1) ||
-     (input1 == credentials[1].Name2) &&
-     (input2 == credentials[1].password2) ||
-     (input1 == credentials[2].Name3) &&
-     (input2 == credentials[2].password3) &&
-     (input1 && input2 !== "")
-){
-    alert("login Successfully")   
+  
+  for (let i = 0; i < credentials.length; i++) {
+    let current_user=credentials[i];
+
+    if((current_user.name == input1) &&
+      (current_user.password == input2))
+    {
+      window.location = "dashboard.html" 
     }
- else{
-  alert("Login failed")
- }
+  }
 }
